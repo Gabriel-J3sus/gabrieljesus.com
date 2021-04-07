5c43440ae9794af25f2a00bf3c86ba3b513ef51f
Gabriel-J3sus <gabr.jesus001@gmail.com>
2021-04-07 19:09:15 -0300

    feat: adding error 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-use-before-define */
+import React from 'react'
+
+import { Container } from '../styles/pages/404'
+
+const Custom404: React.FC = () => {
+  return (
+    <Container>
+      <h1>404 - Page Not Found</h1>
+    </Container>
+  )
+}
+
+export default Custom404
